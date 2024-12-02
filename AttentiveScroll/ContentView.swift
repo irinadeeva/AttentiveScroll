36bb8bb50f5226d220d382652aba6adc4f350d98
irinadeeva <deeva97@me.com>
2024-12-02 20:09:16 +0300

    done

--- AttentiveScroll/ContentView.swift
+++ AttentiveScroll/ContentView.swift
@@ -6,81 +6,53 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+  var body: some View {
+    TabView {
+      ScrollViewTab(content: "Tab 1 Content")
+        .tabItem {
+          Label("First", systemImage: "star.fill")
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+  }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct ScrollViewTab: View {
+  let content: String
+
+  var body: some View {
+
+    GeometryReader { geometry in
+
+      ZStack(alignment: .bottom) {
+        ScrollView {
+          VStack {
+            ForEach(0..<100) { index in
+              Text("\(index + 1)")
+                .frame(maxWidth: .infinity)
+            }
+          }
+        }
+        .safeAreaInset(edge: .bottom) {
+          Color.clear.frame(height: 50)
+        }
+
+        GeometryReader { geometry in
+          Color.red
+            .frame(height: geometry.size.height)
+            .ignoresSafeArea()
+            .opacity(0.5)
+        }
+        .frame(height: 50)
+      }
+      .scrollIndicators(.visible)
+    }
+  }
+}
+
 
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+  ContentView()
 }
